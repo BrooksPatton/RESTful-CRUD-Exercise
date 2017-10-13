3c7316c5c13c24ed4833ab10d93f14e2ce80c837
dfrancese <davefrancese@gmail.com>
2017-10-13 13:35:36 -0600

    completed exercise

--- db/query.js
+++ db/query.js
@@ -1,28 +1,36 @@
-var db = require('./connection');
+const db = require('./connection');
 
 function getAll() {
 	// Get all cartoons and return them to the route
-	return pg.select().from('cartoons');
+	return db('cartoon');
 }
 
 function getOne(id) {
 	// Get one cartoon by the id and return it
-	return pg.select().from('cartoons').where('id', id);
+	return db('cartoon').where('id', id);
 }
 
-function create(cartoon) {
+function create(cartoon, year, network) {
 	// Add a cartoon to the database
-	return pg.select().from('cartoons').insert({title: cartoon})
+	return db.select('*').from('cartoon').insert({
+		title: cartoon,
+		year: year,
+		network: network
+	})
 }
 
-function update(id, cartoon) {
+function update(id, cartoon, year, network) {
 	// Update a cartoon with the specified id
-	return pg.select().from('cartoons').where('cartoon', cartoon).update('id', id)
+	return db.select('*').from('cartoon').where('title', cartoon).update({
+		'id': id,
+		'year': year,
+		'network': network
+	})
 }
 
 function remove(id) {
 	// Delete the specified cartoon
-	return pg.select().from('cartoons').where('id', id).del();
+	return db.select('*').from('cartoon').where('id', id).del();
 }
 
 module.exports = {
--- routes/cartoons.js
+++ routes/cartoons.js
@@ -1,32 +1,45 @@
 const express = require('express');
 const router = express.Router();
-
-// Bring in the queries object that has all of the db query functions
-const query = require("../db/query");
+const query = require("../db/query");// Bring in the queries object that has all of the db query functions
 
 // Create the route for get all
 router.get("/", (request, response, next) => {
     query.getAll()
-    .then(cartoons => {
-        response.json(cartoons);
+    .then(cartoon => {
+        response.json(cartoon);
     });
 });
 
 // Create the route for get one
 router.get("/:id", (request, response, next) => {
-    query.getOne(request.params.id)
-    .then(cartoons => {
-        response.json(cartoons);
+    query.getOne(3)
+    .then((cartoon) => {
+        response.json(cartoon);
     });
 });
 
 // Create the route for creating an item
-
+router.post("/", (request, response, next) => {
+    query.create('Daffy Duck', '1937', 'Warner Bros.')
+    .then((cartoon) => {
+        response.json(cartoon);
+    });
+});
 
 // Create the route for updating
-
+router.patch("/", (request, response, next) => {
+    query.update(4, 'Daffy Duck', '1937','Warner Brothers')
+    .then((cartoon) => {
+        response.json(cartoon);
+    });
+});
 
 // Route for deleting an item
-
+router.delete("/", (request, response, next) => {
+    query.remove(8)
+    .then((cartoon) => {
+        response.json(cartoon);
+    });
+});
 
 module.exports = router;
