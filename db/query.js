90bad30951c414aa5dbe22a0404c949dbda8829d
dfrancese <davefrancese@gmail.com>
2017-10-13 11:09:21 -0600

    'query done/ not checked'

--- db/query.js
+++ db/query.js
@@ -2,22 +2,27 @@ var db = require('./connection');
 
 function getAll() {
 	// Get all cartoons and return them to the route
+	return pg.select().from('cartoons');
 }
 
 function getOne(id) {
 	// Get one cartoon by the id and return it
+	return pg.select().from('cartoons').where('id', id);
 }
 
 function create(cartoon) {
 	// Add a cartoon to the database
+	return pg.select().from('cartoons').insert({title: cartoon})
 }
 
 function update(id, cartoon) {
 	// Update a cartoon with the specified id
+	return pg.select().from('cartoons').where('cartoon', cartoon).update('id', id)
 }
 
 function remove(id) {
 	// Delete the specified cartoon
+	return pg.select().from('cartoons').where('id', id).del();
 }
 
 module.exports = {
--- routes/cartoons.js
+++ routes/cartoons.js
@@ -1,12 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
 // Bring in the queries object that has all of the db query functions
+const query = require("../db/query");
 
 // Create the route for get all
-
+router.get("/", (request, response, next) => {
+    query.getAll()
+    .then(cartoons => {
+        response.json(cartoons);
+    });
+});
 
 // Create the route for get one
-
+router.get("/:id", (request, response, next) => {
+    query.getOne(request.params.id)
+    .then(cartoons => {
+        response.json(cartoons);
+    });
+});
 
 // Create the route for creating an item
 
