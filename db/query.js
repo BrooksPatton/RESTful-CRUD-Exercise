a3c00c9274551d265c9e749671f1a86d60ab9743
Makayla Clausen <mclausen792@gmail.com>
2017-10-13 11:06:57 -0600

    My Solutions

--- db/query.js
+++ db/query.js
@@ -1,29 +1,34 @@
 var db = require('./connection');
 
 function getAll() {
-	// Get all cartoons and return them to the route
+  // Get all cartoons and return them to the route
+  return db.select().from('cartoon')
 }
 
 function getOne(id) {
-	// Get one cartoon by the id and return it
+  // Get one cartoon by the id and return it
+  return db.select().from('cartoon').where('id', id)
 }
 
 function create(cartoon) {
-	// Add a cartoon to the database
+  // Add a cartoon to the database
+  return db.table('cartoon').insertInto(title, year, network)
 }
 
 function update(id, cartoon) {
-	// Update a cartoon with the specified id
+  // Update a cartoon with the specified id
+  return db.update('cartoon').from('cartoon').where('id', id)
 }
 
 function remove(id) {
-	// Delete the specified cartoon
+  // Delete the specified cartoon
+  return db.delete().from('cartoon').where('id', id)
 }
 
 module.exports = {
-	getAll,
-	getOne,
-	create,
-	update,
-	remove
+  getAll,
+  getOne,
+  create,
+  update,
+  remove
 };
--- routes/cartoons.js
+++ routes/cartoons.js
@@ -1,20 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var queries = require('../db/query')
 // Bring in the queries object that has all of the db query functions
 
 // Create the route for get all
-
+router.get('/', function(request, response, next) {
+  queries.getAll()
+    .then(function(cartoon) {
+      response.json(cartoon)
+    })
+})
 
 // Create the route for get one
+router.get('/:id', function(request, response, next) {
+  queries.getOne(request.params.id)
+    .then(function(cartoon) {
+      response.json(cartoon)
+    })
+})
 
 
 // Create the route for creating an item
+router.post('/', function(request, response, next) {
+  queries.create(cartoon)
+    .then(function(cartoon) {
+      response.json(cartoon)
+    })
+})
 
 
 // Create the route for updating
+router.put('/:id', function(request, response, next) {
+  queries.update(request.params.id)
+    .then(function(cartoon) {
+      response.send(cartoon)
+    })
+})
 
 
 // Route for deleting an item
+router.delete('/:id', function(request, response, next) {
+  queries.remove(request.params.id)
+    .then(function(cartoon) {
+      response.send(cartoon)
+    })
+})
 
 
 module.exports = router;
