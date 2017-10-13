c4618592c8d3e9153e6a209ec2044492e8abbda3
Calvin Kreusser <CKreusser@gmail.com>
2017-10-13 14:21:56 -0600

    All functions functioning... HUZZAH

--- db/query.js
+++ db/query.js
@@ -7,18 +7,23 @@ function getAll() {
 }
 
 function getOne(id) {
+		return pg('cartoon').first().where('id', id);
 	// Get one cartoon by the id and return it
 }
 
 function create(cartoon) {
+	return pg('cartoon').insert(cartoon)
 	// Add a cartoon to the database
 }
 
 function update(id, cartoon) {
+	return pg('cartoon').update(cartoon).where('id', id)
 	// Update a cartoon with the specified id
 }
 
 function remove(id) {
+	return pg('cartoon').where('id', id).del()
+
 	// Delete the specified cartoon
 }
 
--- routes/cartoons.js
+++ routes/cartoons.js
@@ -1,28 +1,46 @@
 var express = require('express');
 var router = express.Router();
-const query = require('../db/query');
 
 // Bring in the queries object that has all of the db query functions
+const query = require('../db/query');
 
 // Create the route for get all
 router.get("/", function(request, response, next) {
     query.getAll()
     .then(function(cartoon){
-      console.log(cartoon);
-
-        response.json(cartoon);
+      response.json(cartoon);
     });
   });
 // Create the route for get one
-
+router.get("/:id", function(request, response, next) {
+    query.getOne(request.params.id)
+    .then(function(cartoon){
+      response.json(cartoon);
+    });
+  });
 
 // Create the route for creating an item
-
+router.post('/', function(request, response, next) {
+  query.create(request.body)
+  .then(function(cartoon) {
+    response.json(cartoon)
+  })
+})
 
 // Create the route for updating
-
+router.put('/', function(request, response, next) {
+  query.update(request.body.id, request.body)
+  .then(function(cartoon) {
+    response.send('updated')
+  })
+})
 
 // Route for deleting an item
-
+router.delete('/', function(request, response, next) {
+  query.remove(request.body.id)
+  .then(function(cartoon) {
+    response.json(cartoon)
+  })
+})
 
 module.exports = router;
